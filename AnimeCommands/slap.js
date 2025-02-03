f0b1955b25ff9bccdb0b7547edf8da9facf9c8f4
MisterChips <elyeskebaili01@gmail.com>
2025-02-03 18:16:04 +0100

    Update slap.js

--- AnimeCommands/slap.js
+++ AnimeCommands/slap.js
@@ -3,7 +3,7 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'slap',
-  description: 'Give someone a virtual slap!',
+  description: 'Donnez une gifle virtuelle à quelqu'un !',
   async execute(message, args) {
     const sender = message.author;
     const targetUser = message.mentions.users.first();
