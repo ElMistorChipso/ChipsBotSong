16efecb83fdfc55250d9de4e7922ec858a297680
MisterChips <elyeskebaili01@gmail.com>
2025-02-03 18:14:56 +0100

    Update sad.js

--- AnimeCommands/sad.js
+++ AnimeCommands/sad.js
@@ -3,7 +3,7 @@ const anime = require('anime-actions');
 
 module.exports = {
   name: 'sad',
-  description: 'Express sadness with a GIF!',
+  description: 'Exprimez votre tristesse avec un GIF !',
   async execute(message, args) {
     const sender = message.author;
     const sadGif = await anime.sad();
