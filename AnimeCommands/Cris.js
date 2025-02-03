f740f9771d628b47ef37f023a5da89975d35e344
MisterChips <elyeskebaili01@gmail.com>
2025-02-03 18:39:20 +0100

    Update Cris.js

--- AnimeCommands/Cris.js
+++ AnimeCommands/Cris.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
-  name: 'scream',
+  name: 'Cris',
   description: 'Criez de peur ou d’excitation !',
   async execute(message, args) {
     const sender = message.author;
