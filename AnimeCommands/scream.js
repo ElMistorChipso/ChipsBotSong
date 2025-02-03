6548c8802a60ac90b41285688c05c3cd9cc50b41
MisterChips <elyeskebaili01@gmail.com>
2025-02-03 18:15:36 +0100

    Update scream.js

--- AnimeCommands/scream.js
+++ AnimeCommands/scream.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'scream',
-  description: 'Scream in fear or excitement!',
+  description: 'Criez de peur ou d’excitation !',
   async execute(message, args) {
     const sender = message.author;
     const screamGif = await anime.scream();
 
     const embed = new EmbedBuilder()
       .setColor('#ff3300')
-      .setDescription(`${sender} se sent effrayé... 😱`)
+      .setDescription(`${sender} est en train de crier... 😱`)
       .setImage(screamGif);
 
     message.reply({ embeds: [embed] });
