87e42ed68a5b65046206d9f5341dae9cedaa8ffc
MisterChips <elyeskebaili01@gmail.com>
2025-02-03 07:21:05 +0100

    Update blush.js

--- AnimeCommands/blush.js
+++ AnimeCommands/blush.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'blush',
-  description: 'Blush in embarrassment!',
+  description: 'Rougissez d'embarras !',
   async execute(message, args) {
     const sender = message.author;
     const blushGif = await anime.blush();
 
     const embed = new EmbedBuilder()
       .setColor('#ff66b2')
-      .setDescription(`${sender} is blushing... 💖`)
+      .setDescription(`${sender} rougit... 💖`)
       .setImage(blushGif);
 
     message.reply({ embeds: [embed] });
