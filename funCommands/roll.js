c0f977f20d7efbacf79e54ccd1b605e58198efa6
MisterChips <elyeskebaili01@gmail.com>
2025-02-04 07:21:23 +0100

    Update roll.js

--- funCommands/roll.js
+++ funCommands/roll.js
@@ -2,12 +2,12 @@ const db = require("../mongodb");
 module.exports = {
   name: 'roll',
   aliases: ['dice', 'rolldice'],
-  description: 'Rolls a dice',
+  description: 'Rouler un dé',
   execute(message, args) {
     const sides = parseInt(args[0]) || 6; // Default to 6 sides if no argument provided
     const result = Math.floor(Math.random() * sides) + 1;
 
     // Reply to the user with the dice roll result
-    message.reply(`You rolled a ${result} on a ${sides}-sided dice!`);
+    message.reply(`Tu à eu un ${result} sur un dé à faces ${sides}`);
   },
 };
