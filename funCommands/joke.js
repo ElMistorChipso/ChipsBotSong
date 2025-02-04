a94ca2bd37f18ab73ec0b4c328dcae1b12c9eddc
MisterChips <elyeskebaili01@gmail.com>
2025-02-04 07:22:54 +0100

    Update joke.js

--- funCommands/joke.js
+++ funCommands/joke.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const { EmbedBuilder } = require('discord.js');
 const db = require("../mongodb");
 module.exports = {
-  name: 'joke',
-  description: 'Tells a random joke',
+  name: 'Blague',
+  description: 'Dire une blague aléatoire',
   async execute(message, args) {
     try {
       const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
